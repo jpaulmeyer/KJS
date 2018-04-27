56b524803090bf5fd7fd9f4b3977c15a7173416e
kahunuifoster <kahunuifoster@gmail.com>
2018-04-27 15:44:06 -0400

    progress bar

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
 
-function myfunc()
-    {
-    	var s = document.getElementsByName('newSkill')[0];
-		var text = s.options[s.selectedIndex].text;
-		console.log(text)
-    }
+$(document).ready(function() {
+var i = 1;
+$('.progress .circle').removeClass().addClass('circle');
+$('.progress .bar').removeClass().addClass('bar');
+setInterval(function() {
+	if($(location).attr('href') == "jpaulmeyer.github.io/newinterest.html"){
+$('.progress .circle:nth-of-type(' + i + ')').addClass('active');
+
+$('.progress .circle:nth-of-type(' + (i - 1) + ')').removeClass('active').addClass('done');
+
+$('.progress .circle:nth-of-type(' + (i - 1) + ') .label').html('&#10003;');
+
+$('.progress .bar:nth-of-type(' + (i - 1) + ')').addClass('active');
+
+$('.progress .bar:nth-of-type(' + (i - 2) + ')').removeClass('active').addClass('done');
+
+i++;
+}
+if (i == 0) {
+$('.progress .bar').removeClass().addClass('bar');
+$('.progress div.circle').removeClass().addClass('circle');
+i = 1;
+}
+}, 1000);
+console.log($(location).attr('href'))
+});
