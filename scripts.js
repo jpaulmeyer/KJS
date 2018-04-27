9b0b9f91e9e4752431da57fe0d4ccfd2ff58853a
kahunuifoster <kahunuifoster@gmail.com>
2018-04-27 17:48:15 -0400

    progress bar done

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
 
 $(document).ready(function() {
 var i = 1;
-$('.progress .circle').removeClass().addClass('circle');
-$('.progress .bar').removeClass().addClass('bar');
 setInterval(function() {
-	if($(location).attr('href') == "https://jpaulmeyer.github.io/newinterest.html"){
+	if($(location).attr('href') == "file:///Users/kahunuifoster/Documents/GitHub/jpaulmeyer.github.io/newinterest.html"){
 $('.progress .circle:nth-of-type(' + i + ')').addClass('active');
-
+}
 $('.progress .circle:nth-of-type(' + (i - 1) + ')').removeClass('active').addClass('done');
 
 $('.progress .circle:nth-of-type(' + (i - 1) + ') .label').html('&#10003;');
@@ -24,5 +22,4 @@ i = 1;
 }
 }, 1000);
 console.log($(location).attr('href'))
-}
 });
